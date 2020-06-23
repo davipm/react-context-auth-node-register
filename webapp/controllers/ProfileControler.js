3a71b2a4c32df39bbe0e6f2b9e001d257600a1bd
davi <davi.p.m94@gmail.com>
2020-06-22 21:00:52 -0300

    Profile Controller

---
+++ webapp/controllers/ProfileControler.js
@@ -0,0 +1,20 @@
+import connection from "../database/connection";
+
+/**
+ *
+ * @param request
+ * @param response
+ * @returns {Promise<this>}
+ */
+export const getProfiles = async (request, response) => {
+  try {
+    const company_id = request.headers.authorization;
+    const freelancers = await connection("freelancers")
+      .where("company_id", company_id)
+      .select("*");
+
+    return response.status(200).json(freelancers);
+  } catch (error) {
+    return response.status(500).json({ error: "Server Error" });
+  }
+};
