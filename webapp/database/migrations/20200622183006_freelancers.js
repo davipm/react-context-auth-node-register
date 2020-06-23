1abadc3c930b86c9a8651ddd88886832f0b9b7ae
davi <davi.p.m94@gmail.com>
2020-06-22 21:01:59 -0300

    creating migrations

---
+++ webapp/database/migrations/20200622183006_freelancers.js
@@ -0,0 +1,15 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('freelancers', function (table) {
+    table.increments();
+
+    table.string("title").notNullable();
+    table.string("description").notNullable();
+    table.decimal("value").notNullable();
+    table.string("company_id").notNullable();
+    table.foreign("company_id").references("id").inTable("companys");
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('freelancers');
+};
