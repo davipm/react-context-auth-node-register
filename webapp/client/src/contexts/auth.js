81a57206b446f6d8beff1c97aa1738e0887ad239
davi <davi.p.m94@gmail.com>
2020-07-06 01:00:42 -0300

    create initial value

--- webapp/client/src/contexts/auth.js
+++ webapp/client/src/contexts/auth.js
@@ -1,45 +1,51 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useReducer } from "react";
 import { useHistory } from "react-router-dom";
 import { toast } from "react-toastify";
 
 import api from "../services/api";
 
+const initialState = {
+  loading: null,
+  user: localStorage.getItem("name") || null,
+  companyId: localStorage.getItem("companyId") || null
+}
+
 const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
   const history = useHistory();
-  const [user, setUser] = useState(localStorage.getItem("name") || null);
-  const [companyId, setCompanyId] = useState(
-    localStorage.getItem("companyId") || null
+  const [state, setState] = useReducer((state, newState) =>
+    ({...state, ...newState}), initialState
   );
+  const { user } = state;
 
   async function singIn(data) {
+    setState({ loading: true });
+
     try {
       const { data: { name } } = await api.post("/sessions", data);
-      setUser(name);
-      setCompanyId(data.id);
+      setState({ user: name, companyId: data.id });
 
       localStorage.setItem("name", name);
       localStorage.setItem("companyId", data.id);
 
       history.push("/profile");
     } catch (error) {
-      toast.error(`${error}`, {
-        hideProgressBar: true,
-        autoClose: 3000,
-      });
+      toast.error(`${error}`);
     }
+
+    setState({ loading: false });
   }
 
   function singOut() {
     localStorage.clear();
-    setUser(null);
+    setState({ user: null });
     history.push("/");
   }
 
   return (
     <AuthContext.Provider
-      value={{ singned: !!user, user, companyId, singIn, singOut }}
+      value={{ singned: !!user, ...state, singIn, singOut }}
     >
       {children}
     </AuthContext.Provider>
