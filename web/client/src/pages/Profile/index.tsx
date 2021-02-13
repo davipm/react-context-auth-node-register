474ee46b8dbaba8f957e8011bd12aa16ac81c5a3
davi <davi.p.m94@gmail.com>
2021-02-13 13:04:42 -0300

    adding types for freelancer

--- web/client/src/pages/Profile/index.tsx
+++ web/client/src/pages/Profile/index.tsx
@@ -3,9 +3,8 @@ import { Link } from "react-router-dom";
 import { FiPower, FiTrash2 } from "react-icons/fi";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { useQuery, useMutation, useQueryClient } from "react-query";
-import { toast } from "react-toastify";
 
-import api, { loadProfile, deleteProfile } from "../../services/api";
+import { loadProfile, deleteProfile, deleteCompany } from "../../services/api";
 import { useAuth } from "../../contexts/auth";
 import logoImg from "../../images/logo.svg";
 
@@ -18,12 +17,20 @@ import {
 
 import { Button } from "../../components/Button";
 
+export interface Freelancer {
+  id: any;
+  title: string;
+  description: string;
+  value: number;
+  company_id?: string;
+}
+
 export default function Profile() {
   const queryClient = useQueryClient();
   const { user, companyId, singOut } = useAuth();
-  const [activeId, setActiveId] = useState(null);
+  const [activeId, setActiveId] = useState<any>(null);
 
-  const { data: freelancer, isLoading, error } = useQuery(
+  const { data: freelancer, isLoading, error } = useQuery<Freelancer[]>(
     ["profiles", companyId],
     () => loadProfile(companyId)
   );
@@ -34,16 +41,9 @@ export default function Profile() {
     },
   });
 
-  const deleteCompany = useMutation(
-    () => api.delete(`/companys/${companyId}`),
-    {
-      onSuccess: () => {
-        toast.info(`Ong ${user} deleted`);
-        singOut();
-      },
-      onError: () => toast.error("error deleting Company, try again"),
-    }
-  );
+  const onDeleteCompany = useMutation(() => deleteCompany(user, companyId), {
+    onSuccess: () => singOut!(),
+  });
 
   return (
     <ProfileContainer>
@@ -57,7 +57,7 @@ export default function Profile() {
 
         <div>
           <ProfileButton
-            onClick={() => deleteCompany.mutate({})}
+            onClick={() => onDeleteCompany}
             aria-label="Delete Ong"
             title="Delete Company"
           >
@@ -75,43 +75,44 @@ export default function Profile() {
         {isLoading && !error && <h3>Loading...</h3>}
         {error && <h3>Error!</h3>}
 
-        {freelancer?.map((item) => (
-          <li key={item.id}>
-            <strong>NAME:</strong>
-            <p>{item.title}</p>
+        {!error &&
+          freelancer?.map((item) => (
+            <li key={item.id}>
+              <strong>NAME:</strong>
+              <p>{item.title}</p>
 
-            <strong>DESCRIPTION:</strong>
-            <p>{item.description}</p>
+              <strong>DESCRIPTION:</strong>
+              <p>{item.description}</p>
 
-            <strong>PRICE/H:</strong>
-            <p>
-              {Intl.NumberFormat("pt-BR", {
-                style: "currency",
-                currency: "BRL",
-              }).format(item.value)}
-            </p>
+              <strong>PRICE/H:</strong>
+              <p>
+                {Intl.NumberFormat("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                }).format(item.value)}
+              </p>
 
-            <button
-              type="button"
-              onClick={() => {
-                onDelete.mutate(item.id);
-                setActiveId(item.id);
-              }}
-              aria-label="Delete Freelancer"
-              title="Delete Freelancer"
-            >
-              {onDelete.isLoading && activeId === item.id ? (
-                <AiOutlineLoading3Quarters
-                  size={20}
-                  color="#536DFE"
-                  className="loading"
-                />
-              ) : (
-                <FiTrash2 size={20} color="#536DFE" />
-              )}
-            </button>
-          </li>
-        ))}
+              <button
+                type="button"
+                onClick={() => {
+                  onDelete.mutate(item.id);
+                  setActiveId(item.id);
+                }}
+                aria-label="Delete Freelancer"
+                title="Delete Freelancer"
+              >
+                {onDelete.isLoading && activeId === item.id ? (
+                  <AiOutlineLoading3Quarters
+                    size={20}
+                    color="#536DFE"
+                    className="loading"
+                  />
+                ) : (
+                  <FiTrash2 size={20} color="#536DFE" />
+                )}
+              </button>
+            </li>
+          ))}
       </ProfileList>
     </ProfileContainer>
   );
